d689d29383623c9a878a608ca8fa98e422a005f2
Jay Zhou <spammyjunbai@gmail.com>
2026-07-25 18:04:55 +0800

    1

--- demoTradingApp/src/main/java/project/demotradingapp/controller/HoldingsController.java
+++ demoTradingApp/src/main/java/project/demotradingapp/controller/HoldingsController.java
@@ -1,9 +1,22 @@
 package project.demotradingapp.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import project.demotradingapp.dto.portfolio.HoldingResponse;
+import project.demotradingapp.service.HoldingService;
 
 @RestController
+@RequestMapping("/holdings")
+@RequiredArgsConstructor
 public class HoldingsController {
+    private final HoldingService holdingService;
+
     // GET /holdings
+    @GetMapping("/")
+    public HoldingResponse getHoldings(){
+        return holdingService.getHolding();
+    }
     // GET /holdings/{stockId}
 }

da6de501e31434c53aee5fdaa3fc8129252f940e
Jay Zhou <zhoujunbai123@gmail.com>
2026-09-09 09:16:31 +0800

    1

---
+++ StockRecommendationEngine/src/main/java/project/stockrecommendationengine/StockRecommendationEngineApplication.java
@@ -0,0 +1,13 @@
+package project.stockrecommendationengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockRecommendationEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockRecommendationEngineApplication.class, args);
+    }
+
+}
---
+++ StockRecommendationEngine/src/test/java/project/stockrecommendationengine/StockRecommendationEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package project.stockrecommendationengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockRecommendationEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
